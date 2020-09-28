4427a951cb7c5635770b84139414438a10ce97c3
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-09-28 23:31:20 +0530

    payment controller endpoint implemented

---
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/PaymentController.java
@@ -0,0 +1,48 @@
+package com.upgrad.FoodOrderingApp.api.controller;
+
+import com.upgrad.FoodOrderingApp.api.model.PaymentListResponse;
+import com.upgrad.FoodOrderingApp.api.model.PaymentResponse;
+import com.upgrad.FoodOrderingApp.service.businness.PaymentService;
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+
+    @RequestMapping(method = RequestMethod.GET, path = "/payment", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<PaymentListResponse> getAllPaymentMethods() {
+
+        List<PaymentEntity> paymentEntityList = new ArrayList<>(paymentService.getAllPaymentMethods());
+
+        PaymentListResponse paymentListResponse = new PaymentListResponse();
+
+        for (PaymentEntity paymentEntity : paymentEntityList) {
+
+            PaymentResponse paymentResponse = new PaymentResponse();
+            paymentResponse.setId(UUID.fromString(paymentEntity.getUuid()));
+            paymentResponse.setPaymentName(paymentEntity.getPaymentName());
+            paymentListResponse.addPaymentMethodsItem(paymentResponse);
+
+        }
+
+        return new ResponseEntity<PaymentListResponse>(paymentListResponse, HttpStatus.OK);
+
+    }
+}
